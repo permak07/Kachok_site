fdeefcac3b8b4ffb857de384bed8731fa961a95b
Nikita Permyakov <permakovnikita70@gmail.com>
2026-06-27 23:09:24 +0800

    Modified the user table

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, func, DateTime
+from sqlalchemy import Column, Integer, String, func, DateTime,Boolean
 from app.core.database import Base
 
 class User(Base):
@@ -10,3 +10,5 @@ class User(Base):
     hashed_password=Column(String(255),nullable=False)
     role=Column(String(20),default="user")
     created_at=Column(DateTime,server_default=func.now())
+    email_verified = Column(Boolean, default=False)
+    verification_code = Column(String(10), nullable=True)
