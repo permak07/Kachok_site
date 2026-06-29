418399db0e35f1c58da640c14c86683eb2c9da1e
Nikita Permyakov <permakovnikita70@gmail.com>
2026-06-29 15:12:40 +0800

    Added seed and added lifespan in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.core.database import AsyncSessionLocal
+from app.seed import seed_categories
 from app.routers import auth
 
+# Создание категорий
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    print("[STARTUP] Запуск seed...")
+    async with AsyncSessionLocal() as db:
+        await seed_categories(db)
+    print("[STARTUP] Seed завершён.")
+    yield
+
+# Приложение
 app=FastAPI(
     title="Kachok_API",
     version="0.1.0",
-    description="Backend for the Kachok site"
+    description="Backend for the Kachok site",
+    lifespan=lifespan
 )
 
 # Даёт разрешение на действия
